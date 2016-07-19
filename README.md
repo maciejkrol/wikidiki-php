857f59ee7399c9ce8c7f1f4268d2cc2983b76821
maciejkrol <mail@maciejkrol.pl>
2016-07-19 10:41:36 +0200

    initial

---
+++ README.md
@@ -0,0 +1,45 @@
+# WikiDiki
+
+WikiDiki uses [Wikipedia](https://wikipedia.org/) to translate words across multiple languages.
+
+## Basic usage
+
+WikiDiki usage is super simple.
+
+```PHP
+//instantiate
+$wikidiki = new \maciejkrol\wikidiki\wikidiki ();
+```
+
+When translating you must tell WikiDiki what is the base language. English in this case. WikiDiki uses ISO two letter language codes.
+
+This way you will get all the available translations as an associative array with the language codes as keys. 
+
+```
+$word       =   'Tea';
+$language   =   'en';
+
+$translated = $wikidiki->translate($word, $language);   //array
+```
+
+You can also provide a third argument, a string with a single language code to get a single translated string.
+
+```
+$word       =   'Tea';
+$language   =   'en';
+$to         =   'pl';
+
+$translated = $wikidiki->translate($word, $language, $to);   //string
+```
+
+or an array to retrieve an array of translations.
+
+```
+$word       =   'Tea';
+$language   =   'en';
+$to         =   ['pl', 'it'];
+
+$translated = $wikidiki->translate($word, $language, $to);   //array
+```
+
+In case WikiDiki can't find a translation ``null`` will be returned;
---
+++ src/wikidiki.php
@@ -0,0 +1,121 @@
+<?php 
+
+namespace maciejkrol\wikidiki;
+
+/**
+ * WikiDiki class lets you translate words (mostly nouns)
+ * across multiple languages based on wikipedia.org
+ */
+class wikidiki {
+
+    /**
+     * Translates the given term from given language.
+     * Last parameter can be used to retrieve only
+     * selected result or results.
+     * @param string $_term
+     * @param string $_from
+     * @param string|array $_to
+     * @return string|array|null
+     */
+    public function translate ($_term, $_from, $_to = null) {
+
+        $url = $this->buildUrl ($_from, $_term);
+        $html = $this->download ($url);
+
+        if ($html === null) {
+            return null;
+        }
+
+        $results = $this->parseResults ($html);
+
+        if ($_to === null) {
+            return $results;
+        } else if (is_array ($_to)) {
+            $fitlered = [];
+            foreach ($_to as $toLang) {
+                $fitlered[$toLang] = isset ($results[$toLang]) ? $results[$toLang] : null;
+            }
+            return $fitlered;
+        } else if (isset ($results[$_to])) {
+            return $results[$_to];
+        } else {
+            return null;
+        }
+    }
+
+    /**
+     * Extracts all results from a given HTML.
+     * @param string $_html
+     * @return array
+     */
+    private function parseResults ($_html) {
+        $regex      = '/(<a href="(.*?)" title="(.*?)" lang="(.*?)" hreflang="(.*?)">(.*?)<\/a>)/i';
+        $matches    = [];
+        preg_match_all ($regex, $_html, $matches);
+
+        if (count ($matches) === 0) {
+            return [];
+        }
+
+        $results = [];
+        for ($i = 0; $i < count($matches[0]); $i++) {
+            $lang           = $matches[4][$i];
+            $href           = $matches[2][$i];
+            $translation    = str_replace ($this->buildURL ($lang), '', $href);
+            $translation    = $this->cleanTerm ($translation);
+            
+            if ($translation !== null) {
+                $results[$lang] = $translation;
+            }
+        }
+
+        return $results;
+    }
+
+    /**
+     * Performs necessary cleaning of an extracted term.
+     * Returns null if term is invalid.
+     * @param string $_term
+     * @return string|null
+     */
+    private function cleanTerm ($_term) {
+        $_term = urldecode ($_term);
+
+        return $_term;
+    }
+
+    /**
+     * Build valid Wikipedia url for a given language and term.
+     * @param string $_lang
+     * @param string $_term
+     * @return string
+     */
+    private function buildUrl ($_lang, $_term = '') {
+        return 'https://'.$_lang.'.wikipedia.org/wiki/'.$_term;
+    }
+
+    /**
+     * Retrieves html from url. Returns null on error.
+     * @param string $_url
+     * @return string|null
+     */
+    private function download ($_url) {
+        $curl = curl_init();
+
+    	curl_setopt ($curl, CURLOPT_URL, $_url);
+	    curl_setopt ($curl, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt ($curl, CURLOPT_FOLLOWLOCATION, true);        
+	    curl_setopt ($curl, CURLOPT_CONNECTTIMEOUT, 5);
+	
+        $data = curl_exec($curl);
+        $code = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+
+        if ($code !== 200) {
+            $data = null;
+        }
+
+    	curl_close($curl);
+
+    	return $data;
+    }
+}
